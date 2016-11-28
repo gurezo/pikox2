2d09a07fd69d11bc880db6b042131ea15e241cb8
gurezo <akihiko.kigure@gmail.com>
2016-11-28 21:41:54 +0900

    ハードウェア変更に伴うモジュール変更

--- js/main.js
+++ js/main.js
@@ -1,30 +1,19 @@
 'use strict';
 
 window.addEventListener('load', function (){
-  // タッチセンサー４つ分の受信処理を行う
-/*
-  var ax = document.querySelector('#ax');
-  var ay = document.querySelector('#ay');
-  var az = document.querySelector('#az');
-*/
 
-  // WebI2C Initialized
-  navigator.requestI2CAccess()
-    .then(function(i2cAccess){
-      var port = i2cAccess.ports.get(0);
-      var grovetouch = new GROVETOUCH(port,0x53);
-      grovetouch.init().then(function(){
+// WebGPIO を起動時に監視
+  navigator.requestGPIOAccess()
+    .then(gpioAccess=>{
+      var port = gpioAccess.ports.get(198);
+      var v = 0;
+      return port.export("out").then(()=>{
         setInterval(function(){
-          grovetouch.read().then(function(values){
-            //定常的にタッチセンサーを監視する
-/*
-            console.log('values(x,y,z):', values.x,values.y,values.z);
-            ax.innerHTML = values.x ? values.x : ax.innerHTML;
-            ay.innerHTML = values.y ? values.y : ay.innerHTML;
-            az.innerHTML = values.z ? values.z : az.innerHTML;
-*/
-          });
-        },1000);
-      })
-    }).catch(e=> console.error('error', e));
+
+          // 画像差し込み
+
+
+        },500);
+      });
+  });
 }, false);
