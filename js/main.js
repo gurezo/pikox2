bc52a0c6f248887f68a40462bac2aca8b3d02d5a
gurezo <akihiko.kigure@gmail.com>
2016-12-01 22:07:47 +0900

    看板側コミット

--- js/main.js
+++ js/main.js
@@ -1,19 +1,31 @@
 'use strict';
 
-window.addEventListener('load', function (){
+var IMAGE_HEIGHT = 100;
+var IMAGE_WIDTH = 100;
 
-// WebGPIO を起動時に監視
-  navigator.requestGPIOAccess()
-    .then(gpioAccess=>{
-      var port = gpioAccess.ports.get(198);
-      var v = 0;
-      return port.export("out").then(()=>{
-        setInterval(function(){
+//レイヤを画面に生成する処理
+function addImage(){
 
-          // 画像差し込み
+  //動的にDiv要素を生成する
+  var elDiv = document.createElement('div');
+  elDiv.style.position = "absolute";
 
+  //10～269の乱数を発生させる
+  var RandLeft = 10 + Math.random()*260;
+  var RandTop = 10 + Math.random()*260;
 
-        },500);
-      });
-  });
-}, false);
+  //の初期表示位置
+  elDiv.style.left = RandLeft ;
+  elDiv.style.top = RandTop ;
+
+  //イメージ画像を読み込む
+  var insImg = document.createElement('img');
+  insImg.src = "./image/ahiru.png";
+  insImg.width = IMAGE_WIDTH;
+  insImg.height = IMAGE_HEIGHT;
+
+  //Divにを組み込む
+  elDiv.appendChild(insImg);
+  //ゲーム画面にボールレイヤ（Div)を組み込む
+  pikox2.appendChild(elDiv);
+}
